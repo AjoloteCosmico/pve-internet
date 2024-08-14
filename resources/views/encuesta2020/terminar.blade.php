2453a313ea1ef91eadf5bfeff2b0e88f08010651
fgonzalez <felmiquiztli@gmail.com>
2024-08-14 12:09:16 -0600

    asf

--- resources/views/encuesta2020/terminar.blade.php
+++ resources/views/encuesta2020/terminar.blade.php
@@ -1,12 +1,47 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-@section('content')
+	<title>Finalizada: Encuesta Seguimiento de Egresados</title>
+	
+	<!--favicon-->
+	<link rel="shortcut icon" type="image/x-icon" href="img/logos/logoPVE-large.png">
+	
+<!-- scripts-->
+<script src="reg.js"></script>
+<script src="maximiza2.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"> </script>
+    <!--CSS-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+	<link rel="stylesheet" href="{{ asset('css/landing.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/encuesta.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+	
+	
+<!--tipografías-->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
     <!--CABECERA/HEADER-->
     <div class="cabecera">
         <div class="logo">
             <a class=logoUNAM href="https://www.unam.mx/"> <img src="/img/logos/logoUNAM-large-azul.png"> </a>
 
-            <a class=lovoPVE href="https://www.pveaju.unam.mx/"> <img src="/img/logos/logoPVE-large.png"> </a>
+            <a class=lovoPVE href="https://www.pveu.unam.mx/"> <img src="/img/logos/logoPVE-large.png"> </a>
         </div>
 
         <div class="subtitulo2 ">
@@ -37,7 +72,7 @@
 
 <div class="finalBtn">
     <div class="boton1">
-        <a href="https://www.pveaju.unam.mx/encuesta/01/seguimiento2024/#resultadosCarrera">FINALIZAR Y SALIR</a>
+        <a href="index.html#resultadosCarrera">FINALIZAR Y SALIR</a>
     </div>
 </div>
 
@@ -47,17 +82,28 @@
 
     <!--CARDS PROPÓSITOS-->
 
-    <div class="propositos">
-
+    <div class="propositosFinal">
         <img class="card" src="/img/gráficos/pag-encuesta-1.png">
 
         <img class="card" src="/img/gráficos/pag-encuesta-2.png">
-        
-        <a href="https://www.pveaju.unam.mx/encuesta/01/seguimiento2024/#resultadosCarrera">
-        <img class="card" src="/img/gráficos/pag-encuesta-3.png"></a>
+
+        <img class="card" src="/img/gráficos/pag-encuesta-3.png">
     </div>
 
 </div>
 </div>
 
-@endsection
+</body>
+
+<!--FOOTER-->
+<footer>
+	<div class="footer">
+
+		<p class="texto6">
+		© Copyright 2024 Universidad Nacional Autónoma de México.
+		<br>
+		Todos los derechos reservados.
+		</p>
+
+	</div>
+</footer>
