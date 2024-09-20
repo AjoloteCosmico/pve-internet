d0dc63b880cf4a2ed0c97deb97ff54e776650ddc
Isaac Celis Vargas <Isaac.celis.3c@gmail.com>
2024-09-20 00:22:38 -0600

    Se identa código y se agrega redirect hacia la encuesta resolviendo el 404

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,21 @@ use App\Http\Controllers\EncuestasController;
 | be assigned to the "web" middleware group. Make something great!         |
 |__________________________________________________________________________|
 */
-Route::get('/encuesta_generacion/{type}', [App\Http\Controllers\Enc20Controller::class, 'inicio'])->name('enc.inicio');
 
-Route::post('/verify_cuenta_2020', [App\Http\Controllers\Enc20Controller::class, 'verify'])->name('enc20.verify');
-Route::get('/encuesta2020/section/{id}/{section}', [App\Http\Controllers\Enc20Controller::class, 'section'])->name('enc20.section');
-Route::post('/update_personal_data/{id}', [App\Http\Controllers\Enc20Controller::class, 'update_personal_data'])->name('enc20.update_personal_data');
-Route::post('/update_section/{id}', [App\Http\Controllers\Enc20Controller::class, 'update'])->name('enc20.update');
-Route::get('/', function () {
-    return redirect(route('login'));
+Route::controller(Enc20Controller::class)->group(function(){
+    Route::get('/encuesta_generacion/{type}', 'inicio')->name('enc.inicio');
+    Route::post('/verify_cuenta_2020',  'verify')->name('enc20.verify');
+    Route::get('/encuesta2020/section/{id}/{section}',  'section')->name('enc20.section');
+    Route::post('/update_personal_data/{id}', 'update_personal_data')->name('enc20.update_personal_data');
+    Route::post('/update_section/{id}','update')->name('enc20.update');
 });
+
+
+Route::get('/', function () {
+    return redirect(route('enc.inicio',[2020]));
+});
+
+
 Auth::routes();
 Route::group(['middleware' => ['auth']], function()
 {   
