613582193c34aefbc25fdd6dff66b11560edb8ce
AjoloteCosmico <72818026+AjoloteCosmico@users.noreply.github.com>
2026-03-26 11:49:30 -0600

    marcar status eg_muestra

--- app/Http/Controllers/EncVerdeController.php
+++ app/Http/Controllers/EncVerdeController.php
@@ -98,7 +98,12 @@ public function verify(Request $request){
         $Egresado=Egresado::where('cuenta',$Encuesta->cuenta)->where('carrera',$Encuesta->nbr2)->first();
         $Encuesta->update($filteredArray);
         $Encuesta->save();
-
+        //actualizar tabla egresado_muestra status=2
+        $EgMuestra=DB::table('egresado_muestra')
+                ->where('egresado_id',$Egresado->id)
+                ->where('muestra_id',897) //ID de muestra de educación continua
+                ->update(['status' => 2,
+                'updated_at'=>now()]);
         //return personal data update with mesage
         return redirect()->route('enc_verde.section',['personal_data',$Encuesta->id]);
             // return view('encuesta2016.terminar',compact('Encuesta'));
