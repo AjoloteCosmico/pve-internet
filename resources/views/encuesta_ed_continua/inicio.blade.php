1d10bf10f1457f16add616ca6183f50315b87d65
AjoloteCosmico <72818026+AjoloteCosmico@users.noreply.github.com>
2026-04-09 18:18:46 -0600

    commit para subir a producción, cambios en la redaccion del texto al  inicio de ed_continua

--- resources/views/encuesta_ed_continua/inicio.blade.php
+++ resources/views/encuesta_ed_continua/inicio.blade.php
@@ -53,7 +53,7 @@ El Programa de Vinculación con los Egresados y Académicos jubilados  de la Uni
         </p>
         <br><br><br>
         <p class="texto2">
-            Como parte de la campaña ReUNAMos Saberes, invitamos a nuestras y nuestros egresados a participar 
+            Como parte de la campaña <span style="color: #e6af2b">reUNAMmos</span>  Saberes, invitamos a nuestras y nuestros egresados a participar 
             en esta encuesta que busca fortalecer el vínculo con la comunidad universitaria y enriquecer la oferta de educación continua.
             <br>
             Le solicitamos su apoyo para responder el siguiente cuestionario, cuyo propósito es conocer:
